3c91dd7d3cb92603774c8cd1b4d2a2e98d08d3cc
André Tavares Machado <andre266@outlook.com.br>
2020-02-25 18:30:57 -0300

    Testes iniciais com movimento das pessas.

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -17,8 +17,16 @@ namespace Xadrez_console
                 {
                     Console.Clear();
                     Tela.imprimeTabuleiro(partida.Tab);
+                    Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.Tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimeTabuleiro(partida.Tab, posicoesPossiveis);
+
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
                     partida.executaMovimento(origem, destino);
--- Xadrez-console/xadrez/Rei.cs
+++ Xadrez-console/xadrez/Rei.cs
@@ -9,6 +9,76 @@ namespace xadrez
         {
         }
 
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = Tab.peca(pos);
+            return p == null || p.Cor != Cor;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tab.Linhas, Tab.Colunas];
+            Posicao pos = new Posicao(0, 0);
+
+            // Acima
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // Nordeste
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // Direita
+            pos.definirValores(Posicao.Linha, Posicao.Coluna + 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // Sudeste
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // Abaixo
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // Sudoeste
+            pos.definirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // Esquerda
+            pos.definirValores(Posicao.Linha, Posicao.Coluna - 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // Noroeste
+            pos.definirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            if (Tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            return mat;
+        }
+
         public override string ToString()
         {
             return "R";
